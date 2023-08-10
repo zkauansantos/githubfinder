560e44b9121aaff1a6692cd90c7d86b865b107e5
Kauan Santos <kauansantosdepontes@gmail.com>
2023-08-10 20:17:40 -0300

    feat: created routes, home and results components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ResultsComponent } from './components/results/results.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'results', component: ResultsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
