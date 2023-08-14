eb9a4ca259ee5bae37e8d7bfeec5875f1156bf41
Kauan Santos <kauansantosdepontes@gmail.com>
2023-08-14 17:08:02 -0300

    fix: created pipes for get data last update repository

---
+++ src/app/pipes/get-last-update-repo.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GetLastUpdateRepoPipe } from './get-last-update-repo.pipe';
+
+describe('GetLastUpdateRepoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new GetLastUpdateRepoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
